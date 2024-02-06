fc7dec14ff4e8d5955e76e7194eed3d418fa6a69
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2024-02-06 23:27:01 +0530

    Time: 29 ms (26.33%) | Memory: 76.1 MB (12.89%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+   
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2){
+        ListNode *temp1=l1,*temp2=l2,*prev=NULL,*temp,*head1,*head2;
+        long long sum=0,carry=0,sum1=0,sum2=0,c=0,c1=0,c2=0;
+        head1=l1;
+        head2=l2;
+        ListNode *x=new ListNode(0);
+        ListNode *ans=x;
+        while(head1!=NULL && head2!=NULL)
+        {
+            sum=(head1->val)+(head2->val)+carry;
+            cout<<sum<<" ";
+            carry=sum/10;
+            sum=sum%10;
+            head1=head1->next;
+            head2=head2->next;
+            ListNode * one=new ListNode(sum);
+            x->next=one;
+            x=x->next;
+            
+        }
+        while(head1!=NULL)
+        {
+            sum=(head1->val)+carry;
+            carry=sum/10;
+            sum=sum%10;
+            head1=head1->next;
+            ListNode * one=new ListNode(sum);
+            x->next=one;
+            x=x->next;
+        }
+        while(head2!=NULL)
+        {
+            sum=(head2->val)+carry;
+            carry=sum/10;
+            sum=sum%10;
+            head2=head2->next;
+            ListNode * one=new ListNode(sum);
+            x->next=one;
+            x=x->next;
+        }
+        if(carry!=0)
+        {
+            ListNode * one=new ListNode(carry);
+            x->next=one;
+            x=x->next;
+        }
+        return ans->next;
+    }
+};
