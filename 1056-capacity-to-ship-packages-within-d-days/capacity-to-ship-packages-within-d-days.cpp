c976dec891fd87ca9fce67442bc8ee1b65ad74cd
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2024-06-21 15:00:42 +0530

    Time: 30 ms (95.64%) | Memory: 33.4 MB (70.01%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int total=0;
+        for(int i=0;i<weights.size();i++)
+        {
+            total+=weights[i];
+        }
+        int low=1;
+        int high=total;
+        while(low<=high)
+        {
+            int sum=0;
+            int c=0;
+            int mid=low+(high-low)/2;
+            int flag=0;
+            cout<<mid<<" ";
+            for(int i=0;i<weights.size();i++)
+            {
+                sum+=weights[i];
+                if(weights[i]>mid)
+                {
+                    low=mid+1;
+                    flag=1;
+                    break;
+                    
+                }
+                 if(sum>mid)
+                 {
+                    if(weights[i]<=mid)
+                    {
+                    c+=1;
+                    }
+                    sum=weights[i];
+                 }
+                 
+            }
+            if(flag==1)
+            {
+                continue;
+            }
+            if(weights[weights.size()-1]<=mid)
+            {
+                c+=1;
+            }
+            if(c<=days)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+            cout<<"\n";
+          
+        }
+        return low;
+    }
+};
