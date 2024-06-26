b616d1cf0ab363157a637a77b20459b4afad1443
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2024-06-26 12:05:59 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (22.73%) - LeetSync

---
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        vector<int>ans;
+        sort(nums.begin(),nums.end()) ;
+        int l=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        int u=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+        u--;
+        if((l<0 || l>=nums.size() )|| nums[l]!=target)
+        {
+            l=-1;
+        }
+        if((u<0 ||u>=nums.size())||nums[u]!=target)
+        {
+            u=-1;
+        }
+        if(l!=-1 || u!=-1)
+        {
+            for(int i=l;i<=u;i++)
+            {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+
+
+   }
+};
