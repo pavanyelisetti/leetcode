758eb634122d1897f2bc13087b7841925425e6d8
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2024-06-28 20:40:10 +0530

    Time: 98 ms (74.54%) | Memory: 67.7 MB (37.58%) - LeetSync

---
+++ 2591-frog-jump-ii/frog-jump-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxJump(vector<int>& a) {
+        int cost=a[1]-a[0];
+        for(int i=2;i<a.size();i++)
+        {
+            cost=max(cost,a[i]-a[i-2]);
+        }
+        return cost;
+
+        
+    }
+};
