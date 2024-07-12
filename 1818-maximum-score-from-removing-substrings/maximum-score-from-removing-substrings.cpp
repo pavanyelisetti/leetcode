bba4171caea6d5f4436570ca7f5af526209ad62e
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2024-07-12 10:49:43 +0530

    Time: 88 ms (57.72%) | Memory: 29.9 MB (23.98%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,107 +1,64 @@
 class Solution {
 public:
-    int maximumGain(string s, int x, int y) {
+    int ans(string s,char x,char y,int d,int e)
+    {
         stack<char>st;
         int c=0,c1=0;
         string t;
+        for(int i=0;i<s.length();i++)
+            {
+                if(s[i]==x)
+                {
+                    if(!st.empty()&&st.top()==y)
+                    {
+                        c+=1;
+                        st.pop();
+                    }
+                    else
+                    {
+                        st.push(s[i]);
+                    }
+                }
+                else
+                {
+                    st.push(s[i]);
+                }
+            }
+            while(!st.empty())
+            {
+                t+=st.top();
+                st.pop();
+            }
+            reverse(t.begin(),t.end());
+            for(int i=0;i<t.length();i++)
+            {
+                if(t[i]==y)
+                {
+                    if(!st.empty()&&st.top()==x)
+                    {
+                        c1+=1;
+                        st.pop();
+                    }
+                    else
+                    {
+                        st.push(t[i]);
+                    }
+                }
+                else
+                {
+                    st.push(t[i]);
+                }
+            }
+            return (c*d+c1*e);
+        }
+    int maximumGain(string s, int x, int y) {
+
         if(y>x)
         {
-            for(int i=0;i<s.length();i++)
-            {
-                if(s[i]=='a')
-                {
-                    if(!st.empty()&&st.top()=='b')
-                    {
-                        c+=1;
-                        st.pop();
-                    }
-                    else
-                    {
-                        st.push(s[i]);
-                    }
-                }
-                else
-                {
-                    st.push(s[i]);
-                }
-            }
-            while(!st.empty())
-            {
-                t+=st.top();
-                st.pop();
-            }
-            reverse(t.begin(),t.end());
-            for(int i=0;i<t.length();i++)
-            {
-                if(t[i]=='b')
-                {
-                    if(!st.empty()&&st.top()=='a')
-                    {
-                        c1+=1;
-                        st.pop();
-                    }
-                    else
-                    {
-                        st.push(t[i]);
-                    }
-                }
-                else
-                {
-                    st.push(t[i]);
-                }
-            }
-            cout<<c<<" "<<c1;
-            return (c*y+c1*x);
+            return ans(s,'a','b',y,x);
         }
-        else
-        {
-           for(int i=0;i<s.length();i++)
-            {
-                if(s[i]=='b')
-                {
-                    if(!st.empty()&&st.top()=='a')
-                    {
-                        c1+=1;
-                        st.pop();
-                    }
-                    else
-                    {
-                        st.push(s[i]);
-                    }
-                }
-                else
-                {
-                    st.push(s[i]);
-                }
-            }
-            while(!st.empty())
-            {
-                t+=st.top();
-                st.pop();
-            }
-            reverse(t.begin(),t.end());
-            for(int i=0;i<t.length();i++)
-            {
-                if(t[i]=='a')
-                {
-                    if(!st.empty()&&st.top()=='b')
-                    {
-                        c+=1;
-                        st.pop();
-                    }
-                    else
-                    {
-                        st.push(t[i]);
-                    }
-                }
-                else
-                {
-                    st.push(t[i]);
-                }
-            }
-            cout<<c<<" "<<c1;
-            return (c*y+c1*x);
-        }
+            return ans(s,'b','a',x,y);
+           
         
     }
 };
