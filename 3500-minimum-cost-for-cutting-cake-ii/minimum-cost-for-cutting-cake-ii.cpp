9b3690a4f8943708b92ff768ea20c8af1d813a6d
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2024-07-16 12:01:08 +0530

    Time: 329 ms (79.42%) | Memory: 233.5 MB (96.43%) - LeetSync

---
+++ 3500-minimum-cost-for-cutting-cake-ii/minimum-cost-for-cutting-cake-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    long long minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
+        sort(horizontalCut.begin(),horizontalCut.end());
+        sort(verticalCut.begin(),verticalCut.end());
+        long long ans=0,h=1,v=1;
+        int i=horizontalCut.size()-1;
+        int j=verticalCut.size()-1;
+        while(i>=0 and j>=0)
+        {
+            if(verticalCut[j]>=horizontalCut[i])
+            {
+                ans+=verticalCut[j]*v;
+                h++;
+                j--;
+            }
+            else
+            {
+                ans+=horizontalCut[i]*h;
+                v++;
+                i--;
+            }
+        }
+        while(i>=0)
+        {
+            ans+=horizontalCut[i]*h;
+            i--;
+        }
+        while(j>=0)
+        {
+            ans+=verticalCut[j]*v;
+                j--;
+        }
+        return ans;
+        
+    }
+};
