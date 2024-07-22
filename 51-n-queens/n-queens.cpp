f09598188dadafbccd2174ab1974308688ba17c3
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2024-07-22 12:11:59 +0530

    Time: 7 ms (42.91%) | Memory: 9.5 MB (63.59%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,58 +1,60 @@
 class Solution {
 public:
-int safe(int row,int col,vector<string>board,int n)
-{
-    int drow=row;
-    int  dcol=col;
-    while(row>=0 && col>=0)
+    bool safe(int row,int col,vector<string>&board,int n)
     {
-        if(board[row][col]=='Q')return 0;
-        row--;
-        col--;
+        int trow=row;
+        int tcol=col;
+        while(row<n && col>=0)
+        {
+            if(board[row][col]=='Q') return false;
+
+            row++;
+            col--;
+        }
+        row=trow;
+        col=tcol;
+        while(col>=0)
+        {
+            if(board[row][col]=='Q') return false;
+            col--;
+        }
+        col=tcol;
+        while(row>=0 and col>=0)
+        {
+            if(board[row][col]=='Q') return false;
+            col--;
+            row--;
+        }
+        return true;
+        
     }
-    row=drow;
-    col=dcol;
-    while(col>=0)
-    {
-        if(board[row][col]=='Q')return 0;
-        col--;
-    }
-    row=drow;
-    col=dcol;
-    while(row<n && col>=0)
-    {
-        if(board[row][col]=='Q')return 0;
-        row++;
-        col--;
-    }
-    return 1;
-}
-    void solve(int col,int n,vector<string>&board,vector<vector<string>>&ans)
+    void count(int row,int col,int n,vector<vector<string>>&ans,vector<string>&board)
     {
         if(col==n)
         {
             ans.push_back(board);
             return;
         }
-        for(int row=0;row<n;row++)
+        for(row=0;row<n;row++)
         {
             if(safe(row,col,board,n))
             {
                 board[row][col]='Q';
-                solve(col+1,n,board,ans);
+                count(row,col+1,n,ans,board);
                 board[row][col]='.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>ans;
         vector<string>board(n);
         string s(n,'.');
-        for(int i=0;i<n;i++)
+        for(int i=0;i<board.size();i++)
         {
             board[i]=s;
         }
-        solve(0,n,board,ans);
+        vector<vector<string>>ans;
+        count(0,0,n,ans,board);
         return ans;
+        
     }
 };
