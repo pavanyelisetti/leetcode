51909bf082713ce595690973309f364663fb3dfe
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2024-07-23 10:53:41 +0530

    Time: 493 ms (73.08%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -4,4 +4,4 @@ group by
 employee_id having count(primary_flag)=1
 union 
 select employee_id,department_id from Employee a 
-where( primary_flag="Y") order by employee_id;
+where( primary_flag="Y") ;
