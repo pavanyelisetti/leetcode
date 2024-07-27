cec7907bb539f9f8eba61a6286632ec4801149ea
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2024-07-27 12:32:41 +0530

    Time: 385 ms (33.73%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -4,4 +4,4 @@ round(sum(q1.rating/q1.position)/count(q1.result),2)as quality ,
 round(count(q2.rating)/count(q1.result)*100,2) as poor_query_percentage 
 from Queries q1 left join  Queries q2
 on  q1.result =q2.result and q2.rating <3 
-group by query_name  having query_name<>'null';
+group by query_name  having q1.query_name!= "null";
